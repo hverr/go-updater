560883fbfbf7694064326f6e2b41561f94d9d8e0
Henri Verroken <henriverroken@gmail.com>
2015-10-24 00:32:03 +0200

    Define App and Release interface

---
+++ app.go
@@ -0,0 +1,21 @@
+package updater
+
+// App is a generic Go application capapble of querying update
+// information and updating itself.
+type App interface {
+	// Query sould query application information from a remote source.
+	Query() error
+
+	// LatestRelease should return the most recent release of the application
+	// that is available.
+	LatestRelease() Release
+}
+
+// Release represents an application release.
+type Release interface {
+	// Name should return the version name of this release.
+	Name() string
+
+	// Information is some human-readable information for this release.
+	Information() string
+}
