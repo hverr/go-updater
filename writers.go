785ce43985b4fde5285cd5e333fdede6afce4fdb
Henri Verroken <henriverroken@gmail.com>
2015-10-24 18:34:40 +0200

    Add DelayedFile structure

--- writers.go
+++ writers.go
@@ -1,6 +1,7 @@
 package updater
 
 import (
+	"io"
 	"io/ioutil"
 	"os"
 	"sync"
@@ -45,5 +46,80 @@ func (a *FileBuffer) Close() error {
 	if a.handle != nil {
 		return a.handle.Close()
 	}
+	return nil
+}
+
+// DelayedFile is a file that is first written to a temporary location.
+//
+// All writes to a delayed file go to a temporary file. When the file is closed,
+// the data is manually copied to the original destination path.
+//
+// This file type can be used to assure that all data is correctly received from
+// an unreliable source, before the final destination file is written to.
+type DelayedFile struct {
+	path string
+
+	buffer  FileBuffer
+	aborted bool
+
+	copier func(io.Writer, io.Reader) (int64, error)
+}
+
+// NewDelayedFile creates a new delayed file.
+func NewDelayedFile(path string) *DelayedFile {
+	return &DelayedFile{
+		path: path,
+		copier: func(w io.Writer, r io.Reader) (int64, error) {
+			return io.Copy(w, r)
+		},
+	}
+}
+
+// Write data to the temporary file.
+func (f *DelayedFile) Write(b []byte) (int, error) {
+	return f.buffer.Write(b)
+}
+
+// Abort will stop the file from copying its contents to the final destination
+// when the file is closed.
+func (f *DelayedFile) Abort() {
+	f.aborted = true
+}
+
+// Close will close the temporary file, copy its contents and delete it.
+//
+// If Abort was called before closing the file, the contents will not be copied
+// to the final destination.
+func (f *DelayedFile) Close() error {
+	// Delete the temporary file
+	defer os.Remove(f.buffer.Path)
+
+	// Close the temporary file
+	f.buffer.Close()
+
+	// Don't copy if aborted
+	if f.aborted {
+		return nil
+	}
+
+	// Open the destination file
+	dest, err := os.Create(f.path)
+	if err != nil {
+		return err
+	}
+	defer dest.Close()
+
+	// Open the source file
+	source, err := os.Open(f.buffer.Path)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	// Copy
+	if _, err := f.copier(dest, source); err != nil {
+		return err
+	}
+
 	return nil
 }
--- writers_test.go
+++ writers_test.go
@@ -1,6 +1,8 @@
 package updater
 
 import (
+	"errors"
+	"io"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -35,6 +37,7 @@ func TestFileBuffer(t *testing.T) {
 		require.Nil(t, err, "Could not open file: %v", err)
 		defer f.Close()
 		data, err := ioutil.ReadAll(f)
+		assert.Nil(t, err, "Could not read file: %v", err)
 		assert.Equal(t, "hello world", string(data))
 
 		// Clean up
@@ -86,5 +89,133 @@ func TestFileBuffer(t *testing.T) {
 		b := &FileBuffer{}
 		err := b.Close()
 		assert.Nil(t, err)
+	}
+}
+
+func TestDelayedFile(t *testing.T) {
+	// Valid test
+	{
+		// Get filename
+		f, err := ioutil.TempFile("", "testing-")
+		require.Nil(t, err)
+		path := f.Name()
+		f.Close()
+
+		// Write data
+		df := NewDelayedFile(path)
+		_, err = df.Write([]byte("hello world"))
+		err = df.Close()
+		assert.Nil(t, err, "Could not close file: %v", err)
+
+		// Check contents
+		f, err = os.Open(f.Name())
+		require.Nil(t, err, "Could not open file: %v", err)
+		defer f.Close()
+		data, err := ioutil.ReadAll(f)
+		assert.Nil(t, err, "Could not read file: %v", err)
+		assert.Equal(t, "hello world", string(data))
+
+		// Make sure temp file does not exists
+		_, err = os.Stat(df.buffer.Path)
+		assert.True(t, os.IsNotExist(err), "Temporary file was not removed.")
+
+		// Clean up
+		err = os.Remove(path)
+		assert.Nil(t, err, "Could not clean up: %v", err)
+	}
+
+	// Invalid destination file
+	{
+		// Write
+		df := NewDelayedFile("/n/o/n/e/x/i/s/t/i/n/g/file")
+		_, err := df.Write([]byte("hello world"))
+		assert.Nil(t, err, "Could not write to file: %v", err)
+
+		// Close
+		err = df.Close()
+		assert.True(t, os.IsNotExist(err))
+
+		// Make sure temp file does not exists
+		_, err = os.Stat(df.buffer.Path)
+		assert.True(t, os.IsNotExist(err), "Temporary file was not removed.")
+	}
+
+	// Invalid source file
+	{
+		// Get filename
+		f, err := ioutil.TempFile("", "testing-")
+		require.Nil(t, err)
+		path := f.Name()
+		f.Close()
+
+		// Write
+		df := NewDelayedFile(path)
+		_, err = df.Write([]byte("hello world"))
+		assert.Nil(t, err, "Could not write to file: %v", err)
+
+		// Close
+		err = os.Remove(df.buffer.Path)
+		assert.Nil(t, err, "Could not remove temporary file: %v", err)
+		err = df.Close()
+		assert.True(t, os.IsNotExist(err))
+
+		// Make sure temp file does not exists
+		_, err = os.Stat(df.buffer.Path)
+		assert.True(t, os.IsNotExist(err), "Temporary file was not removed.")
+	}
+
+	// Aborted
+	{
+		// Get filename
+		f, err := ioutil.TempFile("", "testing-")
+		require.Nil(t, err)
+		path := f.Name()
+		f.Close()
+		os.Remove(path)
+
+		// Write
+		df := NewDelayedFile(path)
+		_, err = df.Write([]byte("hello world"))
+		assert.Nil(t, err, "Could not write to file: %v", err)
+
+		// Abort
+		df.Abort()
+
+		// Close
+		err = df.Close()
+		assert.Nil(t, err, "Could not close file: %v", err)
+
+		// Make sure temporary file is removed
+		_, err = os.Stat(df.buffer.Path)
+		assert.True(t, os.IsNotExist(err))
+
+		// Make sure the contents were not copied
+		_, err = os.Stat(path)
+		assert.True(t, os.IsNotExist(err))
+	}
+
+	// Faulty copier
+	{
+		// Get filename
+		f, err := ioutil.TempFile("", "testing-")
+		require.Nil(t, err)
+		path := f.Name()
+		f.Close()
+		os.Remove(path)
+
+		// Write
+		df := NewDelayedFile(path)
+		_, err = df.Write([]byte("hello world"))
+		assert.Nil(t, err, "Could not write to file: %v", err)
+
+		// Setup failure
+		testErr := errors.New("Copy test error")
+		df.copier = func(io.Writer, io.Reader) (int64, error) {
+			return 0, testErr
+		}
+
+		// Close
+		err = df.Close()
+		assert.Equal(t, testErr, err)
 	}
 }
